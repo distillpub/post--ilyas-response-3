311f01a675335b8ad8cba80b879503f30298eb53
Gabriel Goh <ggoh@openai.com>
2019-06-19 12:57:14 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-import VisualTOC                      from './diagrams/VisualTOC.html';
+import What from './diagrams/what.svelte';
+import What2 from './diagrams/what2.svelte';
+import What3 from './diagrams/what3.svelte';
 
-// eagerly initialize vtoc  as it's above the fold
-const tocNav = document.getElementById('vtoc');
-const visualTOC = new VisualTOC({target: tocNav});
+async function run(){
+	async function get_ordering() {
+	    const eigs = await (await fetch("https://storage.googleapis.com/clarity-public/ggoh/madry_response/what/eigs.json")).json()
+	    const pareto = await (await fetch("https://storage.googleapis.com/clarity-public/ggoh/madry_response/what/pareto.json")).json()
+	    const pareto_weights = await (await fetch("https://storage.googleapis.com/clarity-public/ggoh/madry_response/what/pareto_weights.json")).json()
+	    const interp = await (await fetch("https://storage.googleapis.com/clarity-public/ggoh/madry_response/what/linear.json")).json()
 
-// lazily initialize any diagram below the fold. E.G:
-// {
-//   const figure = document.getElementById('StyleTransferExamples');
-//   figure.addEventListener("ready", function() {
-//     const styleTransferExamples = new StyleTransferExamples({target: figure});
-//   });
-// }
+	    return [eigs, pareto, pareto_weights, interp]
+	}
+
+	const ordering = get_ordering()
+
+	ordering.then( function (data) {
+
+		var eigs = data[0]
+		var pareto = data[1]
+		var pareto_weights = data[2]
+		var interp = data[3]
+
+		new What({
+			target: document.getElementById('what'),
+			props: {
+				eigs: eigs,
+				pareto: pareto,
+				pareto_weights: pareto_weights
+			}
+		}); 
+
+		new What2({
+			target: document.getElementById('what2'),
+			props: {
+				eigs: eigs,
+				pareto: interp,
+				pareto_weights: pareto_weights
+			}
+		});
+
+		new What3({
+			target: document.getElementById('what3'),
+			props: {
+				eigs: eigs,
+				pareto: interp,
+				pareto_weights: pareto_weights
+			}
+		}); 
+
+	})
+
+}
+
+run()
